ade707032d3ea52d5abd1ffc56f55b7f7135a791
Dylan-Brooks2 <107555660+Dylan-Brooks2@users.noreply.github.com>
2026-07-19 16:36:21 -0400

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        /*
+        k = 0
+        for i to nums.length:
+            if nums[i] !== val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k;
+         */
+        let k = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== val) {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        return k;
+    }
+}

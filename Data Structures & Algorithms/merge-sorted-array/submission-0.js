4321cc2a8b05369b04ca8e2d32702a42140513ff
Dylan-Brooks2 <107555660+Dylan-Brooks2@users.noreply.github.com>
2026-08-17 23:37:33 -0400

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        /**
+        p1 = m - 1 
+        p2 = n - 1
+        p3 = (m + n) - 1
+
+        while p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p1--
+                p3--
+            else:
+                nums1[p3] = nums2[p2]
+                p2--
+                p3--
+         */
+        let p1 = m - 1;
+        let p2 = n - 1;
+        let p3 = (m + n) - 1;
+
+        while (p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p1--;
+                p3--;
+            } else {
+                nums1[p3] = nums2[p2];
+                p2--;
+                p3--;
+            }
+        }
+    }
+}
